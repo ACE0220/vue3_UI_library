064b3f98454dd526e6847749fcd0c6d801d90e72
ace0220 <895047954@qq.com>
2022-08-31 22:56:14 +0800

    feat: jsx的初尝试

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { defineComponent, reactive, ref } from 'vue';
+const img = require('./assets/logo.png'); // eslint-disable-line
+
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      name: 'ace',
+    });
+
+    const numberRef = ref(1);
+
+    function renderHelloWorld(num: number) {
+      return <HelloWorld age={num} />;
+    }
+
+    return () => {
+      const number = numberRef.value;
+      return (
+        <div id="app">
+          <img src={img} alt="" />
+          <p>{state.name + number}</p>
+          {renderHelloWorld(12)}
+        </div>
+      );
+    };
+  },
+});
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
 import { createApp, defineComponent, h, createVNode, reactive, ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
-// import App from './App.vue';
-
-const img = require('./assets/logo.png'); // eslint-disable-line
-
-// h函数和createElement类似
-const App = defineComponent({
-  setup() {
-    const state = reactive({
-      name: 'ace',
-    });
-
-    const numberRef = ref(1);
-
-    setInterval(() => {
-      state.name += '1';
-      numberRef.value += 1;
-    }, 1000);
-
-    // h函数其实是createVNode的简单封装
-    return () => {
-      const number = numberRef.value;
-      return h('div', { id: 'app' }, [h('p', state.name + number)]);
-    };
-  },
-});
+import App from './App';
 
 createApp(App).mount('#app');
