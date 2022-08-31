fe195d477f21abeea1a0b164675409d02deab760
ace0220 <895047954@qq.com>
2022-08-31 16:43:13 +0800

    feat: vue的h函数

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp, defineComponent, h } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+// import App from './App.vue';
+
+const img = require('./assets/logo.png'); // eslint-disable-line
+
+// h函数和createElement类似
+const App = defineComponent({
+  render() {
+    return h('div', { id: 'app' }, [
+      h('img', {
+        alt: 'Vue logo',
+        src: img,
+      }),
+      h(HelloWorld, {
+        msg: 'Welcome to vue.js',
+        age: 12,
+      }),
+    ]);
+  },
+});
 
 createApp(App).mount('#app');
