857af7e351faf743d65cf1c0c8c5d29bbf281e9d
ace0220 <895047954@qq.com>
2022-08-31 20:04:37 +0800

    feat: setup的运用

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12" />
+  {{ name }}:{{ name2 }}:{{ state.name3 }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, computed, watchEffect } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
+  },
+  // setup接受两个参数，props和ctx:{slots, attrs, emit}
+  setup(props, { slots, attrs, emit }) {
+    // ref推荐用于定义基本类型
+    const nameRef = ref('ace1');
+
+    // reactive推荐用于定义复杂类型
+
+    const state = reactive({
+      name3: 'ace3',
+    });
+
+    // computed
+    const computedNameRef = computed(() => {
+      return nameRef.value + '2';
+    });
+
+    // watchEffect里面只有用到了这个ref才会引起副作用
+    watchEffect(() => {
+      console.log(nameRef.value);
+    });
+
+    return {
+      name: nameRef,
+      name2: computedNameRef,
+      state,
+    };
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import { createApp, defineComponent, h } from 'vue';
+import { createApp, defineComponent, h, createVNode } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
-// import App from './App.vue';
+import App from './App.vue';
 
 const img = require('./assets/logo.png'); // eslint-disable-line
 
 // h函数和createElement类似
-const App = defineComponent({
-  render() {
-    return h('div', { id: 'app' }, [
-      h('img', {
-        alt: 'Vue logo',
-        src: img,
-      }),
-      h(HelloWorld, {
-        msg: 'Welcome to vue.js',
-        age: 12,
-      }),
-    ]);
-  },
-});
+// const App = defineComponent({
+//   render() {
+//     // h函数其实是createVNode的简单封装
+//     return createVNode('div', { id: 'app' }, [
+//       createVNode('img', {
+//         alt: 'Vue logo',
+//         src: img,
+//       }),
+//       createVNode(HelloWorld, {
+//         msg: 'Welcome to vue.js',
+//         age: 12,
+//       }),
+//     ]);
+//   },
+// });
 
 createApp(App).mount('#app');
